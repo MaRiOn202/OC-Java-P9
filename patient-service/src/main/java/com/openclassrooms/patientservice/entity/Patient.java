7b59f9eb98d4bb666fbbf6d911871dd5bd78ccf6
Marion <lamacq.marion@hotmail.fr>
2025-07-25 14:32:45 +0200

    lm: maj

--- patient-service/src/main/java/com/openclassrooms/patientservice/entity/Patient.java
+++ patient-service/src/main/java/com/openclassrooms/patientservice/entity/Patient.java
@@ -1,4 +1,17 @@
 package com.openclassrooms.patientservice.entity;
 
+
+
+
+
 public class Patient {
+
+    private String prenom;
+    private String nom;
+    private String dateNaissance;
+    private String genre;
+    private String adresse;
+    private String telephone;
+
+
 }
